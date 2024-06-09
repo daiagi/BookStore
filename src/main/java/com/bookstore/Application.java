a2fa41e6436cb17dc31ddc8c00eb08a68804a815
daiagi <daiagi@gmail.com>
2024-06-09 11:51:45 +0300

    use dotenv

--- src/main/java/com/bookstore/Application.java
+++ src/main/java/com/bookstore/Application.java
@@ -3,9 +3,22 @@ package com.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
+        
         SpringApplication.run(Application.class, args);
     }
 }
