7e58aa8d6fb544e46b6eabad910538a43ed4c467
daiagi <daiagi@gmail.com>
2024-06-09 11:10:56 +0300

    sub route into book-store

--- src/main/java/com/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/book-store/books")
 public class BookController {
     private final BookService bookService;
 
