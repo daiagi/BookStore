9f2f28742adf11e4bc482529d8dc2d947603a053
daiagi <daiagi@gmail.com>
2024-06-16 16:32:20 +0300

    inital shopping cart routes

---
+++ src/main/java/com/bookstore/repository/ShoppingCartRepository.java
@@ -0,0 +1,9 @@
+package com.bookstore.repository;
+
+import com.bookstore.model.ShoppingCart;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface ShoppingCartRepository extends MongoRepository<ShoppingCart, String> {
+    Optional<ShoppingCart> findByUserId(String userId);
+}
