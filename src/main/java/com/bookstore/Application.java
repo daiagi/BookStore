27e037d88328fd57d663daf7a5ca994e68fdd478
daiagi <daiagi@gmail.com>
2024-06-16 10:10:36 +0300

    change dotenv dependency

--- src/main/java/com/bookstore/Application.java
+++ src/main/java/com/bookstore/Application.java
@@ -3,21 +3,21 @@ package com.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
 
         // Load environment variables from .env file
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+        // Dotenv dotenv = Dotenv.load();
+        // System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        // System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        // System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        // System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        // System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        // System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        // System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
 
 
         
