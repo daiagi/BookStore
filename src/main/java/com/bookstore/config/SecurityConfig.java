b489d1ec78a3cafc86b8463302bf6838d08e6f8f
luke <daiagi@gmail.com>
2024-06-29 18:08:49 +0700

    pretty up security config

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -29,20 +29,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // disable CSRF
+        http = http.csrf().disable();
+
+        // Set session management to stateless
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+        // Set permissions on endpoints
         http
-                .csrf().disable()
+
                 .authorizeRequests()
+                // Our public endpoints
                 .antMatchers("/book-store/auth/**").permitAll() // Allow all requests to auth paths
                 .antMatchers(HttpMethod.GET, "/book-store/books/**").permitAll() // Allow read operations
                 .antMatchers("/book-store/health").permitAll() // Allow all requests to "/hello
-                 // .anyRequest().authenticated() Require authentication for all other requests
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new JwtAuthorizationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class);
+                // Our private endpoints
+                .anyRequest().permitAll();
+
+        // Add JWT token filter
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Bean
